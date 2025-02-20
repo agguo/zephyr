531353fff7d0ae5d0e44da958b0760eecbf94a3e
Amy Guo <guoamyag@gmail.com>
2025-02-20 00:33:00 -0500

    created read and write characters functions
    Signed-off-by: Amy Guo <guoamyag@gmail.com>

--- modules/lsh/lsh.h
+++ modules/lsh/lsh.h
@@ -1,6 +1,10 @@
 #ifndef LSH_H
 #define LSH_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <zephyr/posix/unistd.h>
+
 int lsh(void);
 
 #endif /* LSH_H */
---
+++ samples/app_test/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+ #include <stdio.h>
+ #include "../../../modules/lsh/lsh.h"
+ 
+ int main(void)
+ {
+   lsh();
+   return 0;
+ }
